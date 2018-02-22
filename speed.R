6704e20d21fc8927b30ffd916fa333ae435e7475
hagr1dden <hagridden4@gmail.com>
2018-02-22 12:20:03 +0300

    add dist

--- speed.R
+++ speed.R
@@ -3,7 +3,7 @@ library(data.table)
 
 #speed
 
-runDF <- readTCX(file = '/home/evgeny/My_Route.tcx', timezone = "GMT")
+runDF <- readTCX(file = '/home/evgeny/My_Route(1).tcx', timezone = "GMT")
 runTr0 <- trackeRdata(runDF)
 
   plot(runTr0, session = 1)
@@ -12,14 +12,14 @@ runTr0 <- trackeRdata(runDF)
 runSummary <- summary(runTr0)
 print(runSummary)
 plot(runSummary, group = c("total", "moving"), what = c("avgSpeed", "distance", "duration"))
-dev.copy(png, "graph.png")
+#dev.copy(png, "graph.png")
   
 #parts_of_cluster
 
 runDATA <- setDT(runDF)
 
   set.seed(123456789)
-  lm1 = kmeans(runDATA[,c("latitude", "longitude")], 25, nstart = 4, iter.max = 10)
+  lm1 = kmeans(runDATA[,c("latitude", "longitude")], 20, nstart = 1, iter.max = 10)
   plot(runDATA[,c("latitude", "longitude")], col = (lm1$cluster+1), pch = 20, cex = 0.2)
   
 #speed in clusters
@@ -29,20 +29,47 @@ calculate_speed <- function(data, clus){
   for(i in 1:nrow(clus$centers)) {
 
    art <- trackeRdata(subset(data, clus$cluster == i))
-   center <- lm1$centers[i,]
-   points(x = center[1], y = center[2], col = "black", pch = 20, cex = .9)
-   text(x = center[1], y = center[2], pos = 2, cex = 0.5,round(common$avgSpeed, 5))
-   common <- summary(art)
+   center <- clus$centers[i,]
+   common_art <- summary(art)
    #print(common$avgSpeed)
-   mass_speed[i] <- common$avgSpeed
+   points(x = center[1], y = center[2], col = "black", pch = 20, cex = .9)
+   text(x = center[1], y = center[2], pos = 2, cex = 0.5,round(common_art$avgSpeed, 5))
+   mass_speed[i] <- common_art$avgSpeed
   }
   return(mass_speed)
 }
 
+calculate_dist <- function(data, clus){
+  
+  mass_dist <- c()
+    for(i in 1:nrow(clus$centers)) {
+      
+      art <- subset(data[,c("latitude", "longitude")], clus$cluster == i)
+      distantion <- dist(art)
+      mean_cluster <- mean(distantion)
+      mass_dist[i] <- mean_cluster
+    #  print(mass_dist[i])
+    }
+     mean_common <- mean(mass_dist)
+     print(mean_common)
+     for(i in 1:length(mass_dist)){
+      if(mass_dist[i] < mean_common)
+      {
+      points(clus$centers[i,1], clus$centers[i,2], col = "red", pch = 20, cex = 2)
+     }
+    }
+  
+    
+    return(mass_dist)
+  
+}
+
+calculate_dist(runDATA, lm1)
 calculate_speed(runDATA, lm1)
 
-partcluster <- subset(runDATA, lm1$cluster == 1)
+partcluster <- subset(runDATA, lm1$cluster == 20)
 plotRoute(trackeRdata(partcluster), zoom = 13)
-geom_point(lm1, aes(x = lm1$centers[,1], y = lm1$centers[,2]), size = 3)
 
-dev.off()
+l1 <- subset(runDATA[,c("latitude", "longitude")],lm1$cluster == 2)
+m <- mean(dist(l1))
+mx <- data.frame(subset(lm1$centers, lm1$withinss < m))
